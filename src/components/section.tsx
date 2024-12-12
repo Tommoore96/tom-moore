73d765625335d006bd777f56bdfa62abdeff2f5a
Tom Moore <tommoore.dev@gmail.com>
2024-12-12 14:49:25 +0000

    layout change

---
+++ src/components/section.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx'
+import React, { ReactNode } from 'react'
+
+export default function Section({
+  children,
+  className
+}: {
+  children: ReactNode
+  className?: string
+}) {
+  return (
+    <div
+      className={clsx(
+        'flex flex-1 flex-col items-center justify-center gap-4 px-6 pb-4 md:px-11',
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import ExperienceCard from 'components/experience-card'
+import Section from 'components/section'
 import { experience, technologies } from 'data'
 
 export const Route = createLazyFileRoute('/')({
@@ -9,20 +10,20 @@ export const Route = createLazyFileRoute('/')({
 function RouteComponent() {
   return (
     <div className="flex min-h-screen flex-col bg-slate-600">
-      <div className="flex min-h-screen flex-1 flex-col items-center justify-center gap-4 px-6">
+      <Section className="min-h-screen">
         <h1 className="font-display text-5xl">Senior Web Developer</h1>
         <p className="font-body">
           Building clean, reliable, and modern frontend experiences.
         </p>
-        <section className="flex flex-wrap justify-center gap-4">
+        <div className="flex flex-wrap justify-center gap-4">
           {technologies.map((tech) => (
             <span key={tech} className="rounded bg-slate-500 px-2 py-1 text-sm">
               {tech}
             </span>
           ))}
-        </section>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-4 bg-slate-500 px-3 pb-8">
+        </div>
+      </Section>
+      <Section className="bg-slate-500">
         <h2 className="pt-4 font-display text-2xl font-bold">About</h2>
         <p className="font-body">
           Hi, I’m Tom Moore, a frontend developer with 6+ years of experience
@@ -35,15 +36,15 @@ function RouteComponent() {
           mentoring teams, and improving workflows. Let’s collaborate to bring
           your vision to life!
         </p>
-      </div>
-      <div className="flex flex-col items-center justify-center gap-4 bg-slate-600 px-3 pb-8 sm:items-start">
+      </Section>
+      <Section>
         <h2 className="pt-4 font-display text-2xl font-bold">Experience</h2>
         <div className="flex flex-col gap-8">
           {experience.map((exp) => (
             <ExperienceCard key={exp.company} {...exp} />
           ))}
         </div>
-      </div>
+      </Section>
     </div>
   )
 }
