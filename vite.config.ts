ccde69b6f5252dddbd3cc7ff04a80340ae5b558f
Tom Moore <tommoore.dev@gmail.com>
2024-12-19 23:55:02 +0000

    remove vinxi

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,16 @@
 /// <reference types="vitest" />
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { createApp } from 'vinxi'
-import { config } from 'vinxi/plugins/config'
 
-export default createApp({
-  routers: [
-    {
-      name: 'public',
-      type: 'static',
-      dir: './public'
-    },
-    {
-      name: 'client',
-      type: 'spa',
-      handler: './index.html',
-      base: '/',
-      plugins: () => [
-        config('custom', {}),
-        react(),
-        tsconfigPaths(),
-        TanStackRouterVite()
-      ]
-    },
-    {
-      name: 'api',
-      type: 'http',
-      handler: './src/api.ts',
-      base: '/api'
-    }
-  ]
+// https://vitejs.dev/config https://vitest.dev/config
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: '.vitest/setup',
+    include: ['**/test.{ts,tsx}']
+  }
 })
