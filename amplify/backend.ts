05f74b54a60ace377794dc0bdcdb94ac77074177
Tom Moore <tommoore.dev@gmail.com>
2025-01-08 17:20:33 +0000

    import data only

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend'
-import { contactMe } from './functions/contact-me/resource'
+import { data } from './data/resource'
+// import { contactMe } from './functions/contact-me/resource'
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  contactMe
+  data
 })
